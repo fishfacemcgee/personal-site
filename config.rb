617d8af96af189ac06c4ddbd332b11c5ed48213b
Brian Graham <bgrambo@gmail.com>
2014-01-22 03:49:31 +0000

    Initial Commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your prefered out put style here (can be overriden via the command line):
+output_style = :compressed
+
+line_comments = false
